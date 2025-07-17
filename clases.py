cfd9826ef3da2f260bf82065051a9ddfcc76d914
Cristian Cordero <cristiancordero2810@gmail.com>
2025-07-17 02:48:41 -0500

    definición de clases y ejercicio de programación orientada a objetos asignandole cualidades a una entidad creada

--- clases.py
+++ clases.py
@@ -1,3 +1,28 @@
 """
 Clases
 """
+print("--------------------")
+class Programador:
+
+    surname: str = None
+
+    def __init__(self, name: str, age: int, languages: list):
+        self.name = name
+        self.age = age
+        self.languages = languages
+    
+    def print(self):
+        print(
+            f"Nombre: {self.name} | Apellido: {self.surname} | Edad: {self.age} | lenguajes: {self.languages}"
+        )
+
+mi_programador = Programador("Cristian", 25, ["Python", "JavaScript", "PHP"])
+mi_programador.print()
+mi_programador.surname = "Cordero"
+mi_programador.print()
+mi_programador.age = 26
+mi_programador.print()
+mi_programador.languages.append("Java")
+mi_programador.print()
+
+
