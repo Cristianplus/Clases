15b541676771d4dfd6a0669dae2765bd9596d398
Cristian Cordero <cristiancordero2810@gmail.com>
2025-07-17 03:19:28 -0500

    ejercicio extra, implementando las pilas en la construcción de una clase

--- clases.py
+++ clases.py
@@ -25,4 +25,49 @@ mi_programador.print()
 mi_programador.languages.append("Java")
 mi_programador.print()
 
+"""
+Extra
+"""
+print("---------PILA----------")
 
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+    
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if len(self.stack) == 0:
+            return None
+        return self.stack.pop()
+    
+    def count(self):
+        if len(self.stack) == 0:
+            return 0
+        else:
+            return len(self.stack)
+    
+    def print(self):
+        for item in reversed(self.stack):
+            print(item)
+
+my_stack = Stack()
+my_stack.push("A")
+my_stack.push("B")
+my_stack.push("C")
+my_stack.print()
+print(f"1) tienes {my_stack.count()} elementos en la pila")
+print(f"2) se ha eliminado el elemento: {my_stack.pop()} de la pila")
+print(f"3) tienes {my_stack.count()} elementos en la pila")
+my_stack.push("D")
+print("4) se ha añadido el elemento: D a la pila")
+print(f"5) tienes {my_stack.count()} elementos en la pila")
+print(f"6) se ha eliminado el elemento: {my_stack.pop()} de la pila")
+print(f"7) tienes {my_stack.count()} elementos en la pila")
+print("8) los elementos de la pila son:")
+my_stack.print()
+print(f"9) se ha eliminado el elemento: {my_stack.pop()} de la pila")
+print(f"10) se ha eliminado el elemento: {my_stack.pop()} de la pila")
+print(f"11) los elementos de la pila son: {my_stack.count()}")
